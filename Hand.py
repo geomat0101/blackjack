659679722aa5fa275f8c465506e493e4b846152e
Matt George <mdg.home@gmail.com>
2021-11-02 19:54:09 -0400

    initial implementation

--- Hand.py
+++ Hand.py
@@ -5,28 +5,192 @@ from Card import Card
 class Hand (object):
 
     action = {
-        HIT: 1,
-        STAND: 2,
-        SPLIT: 4,
-        DOUBLE: 8,
-        SURRENDER: 16
+        'HIT': 1,
+        'STAND': 2,
+        'SPLIT': 4,
+        'DOUBLE': 8,
+        'SURRENDER': 16
     }
+
 
     def __init__(self):
         self.cards = []
         self.initial_eval = True
         return
 
+
     def __str__(self):
         return(self.cards)
+
 
     def addCard(self, card):
         assert(isinstance(card, Card))
         self.cards.append(card)
+        self.combined_value = sum([x.value for x in self.cards])
+
+        # Soft hand?
+        self.soft_score = None
+        ace_count = len([x for x in self.cards if x.value == 11])
+        if ace_count:
+            self.combined_value = self.combined_value - (10 * ace_count)  # count aces as 1 to start instead of 11
+            if self.combined_value <= 11:
+                self.soft_score = self.combined_value + 10
+            else:
+                self.soft_score = self.combined_value
+        
+
 
     def evaluate(self, upcard):
         if self.initial_eval:
+            self.initial_eval = False
+            card1 = self.cards[0]
+            card2 = self.cards[1]
             # splits?
-            if self.cards[0].value == self.cards[1].value:
+            if card1.value == card2.value:
                 # potential split
-                pass
+                if card1.value in [8, 11]:
+                    # always split pairs of Aces and Eights
+                    return(self.action['SPLIT'])
+                elif card1.value == 10:
+                    # always stand on Tens
+                    return(self.action['STAND'])
+                elif card1.value == 9:
+                    # stand if dealer is showing 7, 10, or A; else split
+                    if upcard.value in [7,10,11]:
+                        return(self.action['STAND'])
+                    else:
+                        return(self.action['SPLIT'])
+                elif card1.value == 7:
+                    # split if dealer is showing 7 or lower; else hit
+                    if upcard.value <= 7:
+                        return(self.action['SPLIT'])
+                    else:
+                        return(self.action['HIT'])
+                elif card1.value == 6:
+                    # split if dealer is showing 3-6; else hit
+                    if upcard.value in [3, 4, 5, 6]:
+                        return(self.action['SPLIT'])
+                    else:
+                        return(self.action['HIT'])
+                elif card1.value == 5:
+                    # double if dealer is showing < 10; else hit
+                    if upcard.value < 10:
+                        return(self.action['DOUBLE'])
+                    else:
+                        return(self.action['HIT'])
+                elif card1.value == 4:
+                    # always hit 4s
+                    return(self.action['HIT'])
+                else:
+                    # split if dealer is showing 4-7; else hit
+                    if upcard.value in [4,5,6,7]:
+                        return(self.action['SPLIT'])
+                    else:
+                        return(self.action['HIT'])
+
+            # not a split
+            # got any aces?
+            ace_offcard = None
+            if card1.value == 11:
+                ace_offcard = card2
+            if card2.value == 11:
+                ace_offcard = card1
+
+            if ace_offcard:
+                return(self.evaluate_soft_hand(ace_offcard.value, upcard.value))
+
+
+            # No splits or Aces
+            # check for double down opportunities
+            if self.combined_value == 9:
+                # Double if dealer is showing less than 7; else hit
+                if upcard.value < 7:
+                    return self.action['DOUBLE']
+                else:
+                    return self.action['HIT']
+            elif self.combined_value == 10:
+                # Double if dealer is showing less than 10; else hit
+                if upcard.value < 10:
+                    return self.action['DOUBLE']
+                else:
+                    return self.action['HIT']
+            elif self.combined_value == 11:
+                # Double if dealer is showing anything except Ace; else hit
+                if upcard.value < 11:
+                    return self.action['DOUBLE']
+                else:
+                    return self.action['HIT']
+            
+            # end of initial eval
+            # no more splits or double downs after initial cards are dealt
+
+        
+        # Soft hand?
+        ace_count = len([x for x in self.cards if x.value == 11])
+        if ace_count:
+            if self.soft_score > self.combined_value:
+                ace_result = self.evaluate_soft_hand(self.soft_score-11, upcard.value)
+                if ace_result == self.action['DOUBLE']:
+                    # can't double after the initial hand
+                    ace_result = self.action['HIT']
+                return(ace_result)
+        
+
+        # if we are down here, then it's not a blackjack or split or double or a soft hand
+        if self.combined_value >= 17:
+            return(self.action['STAND'])
+        elif self.combined_value >= 13:
+            if upcard.value < 7:
+                return(self.action['STAND'])
+            else:
+                return(self.action['HIT'])
+        elif self.combined_value == 12:
+            if upcard.value in [4,5,6]:
+                return(self.action['STAND'])
+            else:
+                return(self.action['HIT'])
+        else:
+            return(self.action['HIT'])
+            
+
+    def evaluate_soft_hand (self, offcard_value, upcard_value):
+        if offcard_value in [2,3]:
+            # Double if dealer is showing 5-6; else hit
+            if upcard_value in [5,6]:
+                return self.action['DOUBLE']
+            else:
+                return self.action['HIT']
+        elif offcard_value in [4,5]:
+            # Double if dealer is showing 4-6; else hit
+            if upcard_value in [4,5,6]:
+                return self.action['DOUBLE']
+            else:
+                return self.action['HIT']
+        elif offcard_value == 6:
+            # Double if dealer is showing 3-6; else hit
+            if upcard_value in [3,4,5,6]:
+                return self.action['DOUBLE']
+            else:
+                return self.action['HIT']
+        elif offcard_value == 7:
+            # Double if dealer is showing 3-6
+            # Stand if dealer is showing 2,7,8; else hit
+            if upcard_value in [3,4,5,6]:                        
+                return self.action['DOUBLE']
+            elif upcard_value in [2,7,8]:
+                return self.action['STAND']
+            else:
+                return self.action['HIT']
+        else:
+            return self.action['STAND']
+
+
+
+
+
+
+
+
+
+
+
--- blackjack.py
+++ blackjack.py
@@ -12,6 +12,7 @@ class BlackJack (object):
         self.num_players = num_players
         self.num_decks = num_decks
         self.shoe = self.getShoe()
+        print(str(self.shoe))
         return
 
 
@@ -24,7 +25,7 @@ class BlackJack (object):
 
 
     def deal (self):
-        if len(self.shoe.discards) > (3 * len(self.shoe)):
+        if len(self.shoe.discards) > (3 * len(self.shoe.cards)):
             # passed 75% shoe penetration; time to reshuffle
             print("Re-Shuffling . . .")
             self.shoe = self.getShoe()
@@ -51,6 +52,8 @@ class BlackJack (object):
         upcard = self.shoe.nextCard()
         dealer_hand.addCard(upcard)
 
+        print("All cards dealt: Dealer showing " + str(upcard))
+
         # dealer blackjack?
         # UNIMPLEMENTED: Insurance bets
         dealer_blackjack = False
@@ -66,23 +69,51 @@ class BlackJack (object):
             for hand in player_hands:
                 if sum([x.value for x in hand.cards]) == 21:
                     # blackjack push
+                    print("Dealer BlackJack: player pushed")
                     pass
                 else:
                     # player hand lost to dealer blackjack
+                    print("Dealer BlackJack: player lost")
                     pass
             return
         
 
         # process player hands
         for hand in player_hands:
+            print("Processing next player hand: %s" % ', '.join([str(x) for x in hand.cards]))
             # blackjack?
             if sum([x.value for x in hand.cards]) == 21:
                 # player blackjack
+                print("Player BlackJack -- Win")
                 next
             
-            # UNIMPLEMENTED: Surrender Hands
-            player_action = hand.evaluate(upcard)
+            player_turn_in_progress = True
+            while player_turn_in_progress:
+                player_action = hand.evaluate(upcard)
+                if player_action == Hand.action['HIT']:
+                    card = self.shoe.nextCard()
+                    message = "Hit: " + str(card) + " -- "
+                    hand.addCard(card)
+                elif player_action == Hand.action['STAND']:
+                    message = "Stand -- "
+                    player_turn_in_progress = False
+#                elif player_action == Hand.action['SPLIT']:
+#                    pass
+                elif player_action == Hand.action['DOUBLE']:
+                    # UNIMPLEMENTED: double the bet
+                    card = self.shoe.nextCard()
+                    message = "Double: " + str(card) + " -- "
+                    hand.addCard(card)
+                    player_turn_in_progress = False
+                else:
+                    raise("Unimplemented player action: " + str(player_action))
 
-        
+                if hand.soft_score:
+                    message += "%d / %d" % (hand.combined_value, hand.soft_score)
+                else:
+                    message += str(hand.combined_value)
+                print(message)
+
+
 if (__name__ == '__main__'):
-    print(BlackJack(num_players=1, num_decks=6).shoe)
+    print(BlackJack(num_players=4, num_decks=6).deal())
