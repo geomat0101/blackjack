c1f78dc7114494929e76e26299227ecf5d4c3fef
Matt George <mdg.home@gmail.com>
2022-07-19 11:13:26 -0400

    Card tests

--- tests/test_Card.py
+++ tests/test_Card.py
@@ -1,5 +1,27 @@
 import pytest
 from blackjack.Card import Card
 
-def test_Card ():
-    assert(str(Card('sPaDeS', 1)) == 'Ace of Spades')
+
+@pytest.mark.parametrize("suit,ranknum", 
+        [   ('spades', 'x'), 
+            ('clubs', 0), 
+            ('hearts', 14),
+            ('cups', 1)
+        ])
+def test_Card_init_input (suit, ranknum):
+    with pytest.raises(AssertionError):
+        Card(suit, ranknum)
+
+
+@pytest.mark.parametrize("ranknum,expected,label",
+        [
+            (1, 11, "Ace"), (2, 2, "Two"), (3, 3, "Three"),
+            (4, 4, "Four"), (5, 5, "Five"), (6, 6, "Six"),
+            (7, 7, "Seven"), (8, 8, "Eight"), (9, 9, "Nine"), 
+            (10, 10, "Ten"), (11, 10, "Jack"),
+            (12, 10, "Queen"), (13, 10, "King")
+        ])
+def test_Card_init (ranknum, expected, label):
+    c = Card('diamonds', ranknum)
+    assert(c.value == expected)
+    assert(str(c) == "%s of Diamonds" % label)
